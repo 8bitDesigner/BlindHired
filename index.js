ac1d99020eb0aee0ef4b529343331b4edc19d1ff
Paul Sweeney <paul@8-bitdesign.com>
2014-10-28 18:24:03 -0700

    Hide profile icon on list and candidate page

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 var imageSelector = '[alt=Profile], [alt=Thumb], [bo_alt="candidate.name"]'
   , nameSelector = '.a--icon + .candidate__info'
 
+function Stylesheet(body) {
+  this.body = body
+  this.tag = null
+}
+
+Stylesheet.prototype.append = function() {
+  var tag = document.createElement('style')
+  tag.appendChild(document.createTextNode(this.body))
+  document.body.appendChild(tag)
+  console.log(tag)
+  this.tag = tag
+}
+
+Stylesheet.prototype.remove = function() {
+  document.body.removeChild(this.tag)
+}
+
 function $(selector) {
   var nodeList = document.querySelectorAll(selector)
   return Array.prototype.slice.call(nodeList)
 }
 
 function anonymize() {
-  $(imageSelector).forEach(function(node) { node.style.display = 'none' })
   $(nameSelector).forEach(function(node) { node.innerText = 'See Candidate' })
 }
 
-setInterval(anonymize, 500)
+function init() {
+  var stylesheet = new Stylesheet(imageSelector + ' { display: none !important }')
+  stylesheet.append()
+  setInterval(anonymize, 500)
+}
+
+init()
